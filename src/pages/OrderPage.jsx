79d0b9847fdfb448561a7d9c022b142621edc766
jsunbin <jsnb6n@gmail.com>
2024-02-05 22:53:46 +0900

    결제 페이지 - 결제수단, 최종 결제정보 마크업

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import OrderItem from '../components/OrderItem';
-import styles from './OrderPage.module.css';
-import mock from '../data/cartItemListMock.json';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import HorizontalRule from '../components/HorizontalRule';
+import styles from './OrderPage.module.css';
+import mock from '../data/cartItemListMock.json';
 
 function OrderPage() {
   const items = mock;
@@ -148,100 +148,114 @@ function OrderPage() {
                 </tbody>
               </table>
 
-              <div>
-                <h3>결제수단</h3>
-                <ul className="pay-method">
-                  <li>
-                    <input
-                      type="radio"
-                      id="payMethodCard"
-                      value="CARD"
-                      name="select-pay-method"
-                    />
-                    <label htmlFor="payMethodCard">신용/체크카드</label>
-                  </li>
-                  <li>
-                    <input
-                      type="radio"
-                      id="payMethodBank"
-                      value="DEPOSIT"
-                      name="select-pay-method"
-                    />
-                    <label htmlFor="payMethodBank">무통장 입금</label>
-                  </li>
-                  <li>
-                    <input
-                      type="radio"
-                      id="payMethodPhone"
-                      value="PHONE_PAYMENT"
-                      name="select-pay-method"
-                    />
-                    <label htmlFor="payMethodPhone">휴대폰 결제</label>
-                  </li>
-                  <li>
-                    <input
-                      type="radio"
-                      id="payMethodNaverPay"
-                      value="NAVERPAY"
-                      name="select-pay-method"
-                    />
-                    <label htmlFor="payMethodNaverPay">네이버페이</label>
-                  </li>
-                  <li>
-                    <input
-                      type="radio"
-                      id="payMethodKakaoPay"
-                      value="KAKAOPAY"
-                      name="select-pay-method"
-                    />
-                    <label htmlFor="payMethodKakaoPay">카카오페이</label>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <h3>최종 결제정보</h3>
-                <ul className={styles['total-payment-box']}>
-                  <li>
-                    <span>총 상품금액</span>
-                    <span>45000원</span>
-                  </li>
-                  <li>
-                    <span>할인금액</span>
-                    <span>0원</span>
-                  </li>
-                  <li>
-                    <span>배송비</span>
-                    <span>2000원</span>
-                  </li>
-                  <li>
-                    <span>최종 결제금액</span>
-                    <span>45000원</span>
-                  </li>
-                  <li>
-                    <div className={styles['agree-box']}>
+              <div className={styles['order-payment-box']}>
+                <div className={styles.left}>
+                  <h3 className={styles['sub-title']}>결제수단</h3>
+                  <ul className={styles['pay-method']}>
+                    <li>
                       <input
-                        type="checkbox"
-                        id="agreeAll"
-                        className="a11y-hidden"
-                        readOnly
+                        type="radio"
+                        id="payMethodCard"
+                        value="CARD"
+                        name="select-pay-method"
                       />
-                      <label
-                        htmlFor="agreeAll"
-                        className={styles['check-label']}
-                      >
-                        <p className={styles['agree-text']}>
-                          주문 내용을 확인하였으며, 정보 제공 등에 동의합니다.
-                        </p>
-                      </label>
+                      <label htmlFor="payMethodCard">신용/체크카드</label>
+                    </li>
+                    <li>
+                      <input
+                        type="radio"
+                        id="payMethodBank"
+                        value="DEPOSIT"
+                        name="select-pay-method"
+                      />
+                      <label htmlFor="payMethodBank">무통장 입금</label>
+                    </li>
+                    <li>
+                      <input
+                        type="radio"
+                        id="payMethodPhone"
+                        value="PHONE_PAYMENT"
+                        name="select-pay-method"
+                      />
+                      <label htmlFor="payMethodPhone">휴대폰 결제</label>
+                    </li>
+                    <li>
+                      <input
+                        type="radio"
+                        id="payMethodNaverPay"
+                        value="NAVERPAY"
+                        name="select-pay-method"
+                      />
+                      <label htmlFor="payMethodNaverPay">네이버페이</label>
+                    </li>
+                    <li>
+                      <input
+                        type="radio"
+                        id="payMethodKakaoPay"
+                        value="KAKAOPAY"
+                        name="select-pay-method"
+                      />
+                      <label htmlFor="payMethodKakaoPay">카카오페이</label>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className={styles.right}>
+                  <h3 className={styles['sub-title']}>최종 결제정보</h3>
+                  <div className={styles['total-payment-box']}>
+                    <ul className={styles['total-order']}>
+                      <li>
+                        <span className={styles['txt-title']}>총 상품금액</span>
+                        <span className={styles['txt-cont']}>
+                          <strong className={styles['txt-num']}>45000</strong>원
+                        </span>
+                      </li>
+                      <li>
+                        <span className={styles['txt-title']}>할인금액</span>
+                        <span className={styles['txt-cont']}>
+                          <strong className={styles['txt-num']}>0</strong>원
+                        </span>
+                      </li>
+                      <li className={styles.delivery}>
+                        <span className={styles['txt-title']}>배송비</span>
+                        <span className={styles['txt-cont']}>
+                          <strong className={styles['txt-num']}>5000</strong>원
+                        </span>
+                      </li>
+                      <HorizontalRule />
+                      <li className={styles.total}>
+                        <span className={styles['txt-title']}>
+                          최종 결제금액
+                        </span>
+                        <span className={styles['txt-cont']}>
+                          <strong className={styles['txt-num']}>50000</strong>원
+                        </span>
+                      </li>
+                    </ul>
+                    <div className={styles.pay}>
+                      <div className={styles['agree-box']}>
+                        <input
+                          type="checkbox"
+                          id="agreeAll"
+                          className="a11y-hidden"
+                          readOnly
+                        />
+                        <label
+                          htmlFor="agreeAll"
+                          className={styles['check-label']}
+                        >
+                          <p className={styles['agree-text']}>
+                            주문 내용을 확인하였으며, 정보 제공 등에 동의합니다.
+                          </p>
+                        </label>
+                      </div>
+
+                      <Button size="large" disabled>
+                        결제하기
+                      </Button>
                     </div>
-                  </li>
-                  <li>
-                    <Button size="large" disabled>
-                      결제하기
-                    </Button>
-                  </li>
-                </ul>
+                  </div>
+                </div>
               </div>
             </form>
           </div>
